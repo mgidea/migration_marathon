42990a7177b174bb2b86adc2caf1f19620812adf
Tom Corley <tomcorleycarpentry@gmail.com>
2013-09-08 22:26:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'd5ad2bae7e4550ee5c7c7685bbf0650333b08aafff5359af2646755b861ae064ac4a77eb6a06063c16f10e8ff67d8a5b23f9b4205de73cb654ca474fa6134b80'
---
+++ db/migrate/20130909021557_add_favorite_to_books.rb
@@ -0,0 +1,11 @@
+class AddFavoriteToBooks < ActiveRecord::Migration
+  def up
+    add_column :books, :favorite, :boolean
+  end
+
+  def down
+    remove_column :books, :favorite
+  end
+end
+
+
